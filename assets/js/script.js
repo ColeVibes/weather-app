80da8465fdbb41c58ac30355a25efb9eb0b946bf
J.Cole <ccjohn015@gmail.com>
2022-01-16 22:11:45 -0600

    lat and long functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,56 @@ var formSubmitHandler = function (event) {
         alert("Please enter a City.") // alert user if they do not enter a city at all
     }
 }
+
+// gets coordinates of city entered in search bar 
+var getCoord = function (city) {
+
+    // format the OpenWeather API URL
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=1b50cbb3b2c102d6f7def4cf1a1ea24d&units=imperial"
+    // console.log(apiUrl)
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                var lon = data.coord.lon
+                var lat = data.coord.lat
+                getWeather(lon, lat)
+
+                var currentCity = data.name
+                currentCityEl.textContent = currentCity
+
+                if (!cities.includes(currentCity)) {
+                    cities.push(currentCity) // push the currentCity into the cities array
+                    saveToSearchHistory() // save to local storage
+                    createButton(currentCity) // creates button for city under search history
+                }
+            })
+        } else {
+            alert("Error: City does no Exist Found. Please check your spelling.")
+        }
+    })
+        .catch(function (error) {
+            alert("Unable to connect to Open Weather Servers")
+        })
+}
+
+// after getting the city coordinates, then we can fetch the "One Call API"
+var getWeather = function (lon, lat) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=1b50cbb3b2c102d6f7def4cf1a1ea24d&units=imperial"
+    // console.log(apiUrl)
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayCurrentWeather(data)
+            })
+        } else {
+            alert("Error: City does not exist. Please check your spelling.")
+        }
+    })
+        .catch(function (error) {
+            alert("Unable to connect to Open Weather Servers")
+        })
+}
