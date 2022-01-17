7ca6d1dc7f267d6d52f57882f926fc2e158edba6
J.Cole <ccjohn015@gmail.com>
2022-01-16 22:27:44 -0600

    fixed variable issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 var cityFormEl = document.querySelector("#city-search")
-var cityInputEl = document.querySelector("#city")
+var cityInputEl = document.querySelector("#userInput")
 var cityContainerEl = document.querySelector("#current-container")
 var searchHistoryEl = document.querySelector("#search-history")
+
+// forcast elements
+var forcastDate = document.querySelector(".date")
+var forcastIcon = document.querySelector(".weather-icon")
+var forcastTemp = document.querySelector(".temp")
+var forcastWind = document.querySelector(".wind")
+var forcastHumidity = document.querySelector(".humidity")
+
 
 // current weather elements
 var currentCityEl = document.querySelector(".current-city")
@@ -10,12 +18,6 @@ var currentWindEl = document.querySelector(".current-wind")
 var currentHumidityEl = document.querySelector(".current-humidity")
 var currentUvEl = document.querySelector(".current-uv")
 
-// forcast elements
-var forcastDate = document.querySelector(".date")
-var forcastIcon = document.querySelector(".weather-icon")
-var forcastTemp = document.querySelector(".temp")
-var forcastWind = document.querySelector(".wind")
-var forcastHumidity = document.querySelector(".humidity")
 
 // create array to hold cities for saving
 var cities = []
